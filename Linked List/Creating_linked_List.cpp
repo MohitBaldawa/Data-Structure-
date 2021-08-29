2c8c63bf5022949bc1b1d734fe892134a0fb3e38
MohitBaldawa <55694540+MohitBaldawa@users.noreply.github.com>
2021-08-30 00:24:15 +0530

    Add files via upload

---
+++ Linked List/Creating_linked_List.cpp
@@ -0,0 +1,35 @@
+//Creation of linked list 
+#include <iostream>
+using namespace std; 
+
+struct Node {
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printhead(Node *head){
+    Node *curr = head;
+
+    while(curr!=NULL)
+    {
+        cout<<(curr->data)<<" ";
+        curr = curr->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node *head = new Node(10);
+    head->next = new Node(30);
+    head->next->next = new Node(40);
+    printhead(head);
+}
+
+
+
